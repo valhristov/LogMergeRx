a37a8b7c0dfb110f41657e81f07ced01699ec855
Valeri Hristov <valeri.hristov@inexto.com>
2021-03-17 15:48:24 +0100

    reinigialize FSW when an error occurs

--- LogMergeRx/Rx/ObservableFileSystemWatcher.cs
+++ LogMergeRx/Rx/ObservableFileSystemWatcher.cs
@@ -9,21 +9,16 @@ namespace LogMergeRx.Rx
 {
     public class ObservableFileSystemWatcher : IDisposable
     {
-        private readonly FileSystemWatcher _fsw;
         private readonly Subject<FilePath> _existing =
             new Subject<FilePath>();
+
+        private FileSystemWatcher _fsw;
 
         public IObservable<FilePath> Changed { get; }
 
         public ObservableFileSystemWatcher(string directoryPath, string filter)
         {
-            _fsw = new FileSystemWatcher
-            {
-                Path = directoryPath,
-                Filter = filter.StartsWith("*") ? filter : $"*{filter}",
-                NotifyFilter = NotifyFilters.LastWrite,
-                IncludeSubdirectories = true,
-            };
+            InitFileSystemWatcher(directoryPath, filter.StartsWith("*") ? filter : $"*{filter}");
 
             var changed = Observable
                 .FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(h => _fsw.Changed += h, h => _fsw.Changed -= h)
@@ -38,6 +33,30 @@ namespace LogMergeRx.Rx
                 .Select(FilePath.FromFullPath);
 
             Changed = Observable.Merge(_existing, changed, created);
+        }
+
+        private void InitFileSystemWatcher(string directoryPath, string filter)
+        {
+            _fsw = new FileSystemWatcher
+            {
+                Path = directoryPath,
+                Filter = filter,
+                NotifyFilter = NotifyFilters.LastWrite,
+                IncludeSubdirectories = true,
+            };
+
+            _fsw.Error += OnFileSystemWatcherError;
+        }
+
+        private void OnFileSystemWatcherError(object sender, ErrorEventArgs e)
+        {
+            var fsw = _fsw;
+
+            InitFileSystemWatcher(fsw.Path, fsw.Filter);
+            
+            fsw.Error -= OnFileSystemWatcherError;
+            fsw.EnableRaisingEvents = false;
+            fsw.Dispose();
         }
 
         public void Start(bool notifyForExistingFiles)
