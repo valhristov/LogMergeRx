e690da08b66e19b23d83c84eff1a14427d17809d
Valeri Hristov <valeri.hristov@inexto.com>
2021-04-20 13:00:19 +0200

    Making text selectable

---
+++ LogMergeRx/MVVM/SelectableTextBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LogMergeRx.MVVM
+{
+    /// <summary>
+    /// https://stackoverflow.com/a/45627524/7156760
+    /// </summary>
+    public class SelectableTextBlock : TextBlock
+    {
+        static SelectableTextBlock()
+        {
+            FocusableProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata(true));
+            TextEditorWrapper.RegisterCommandHandlers(typeof(SelectableTextBlock), true, true, true);
+            // remove the focus rectangle around the control
+            FocusVisualStyleProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata((object)null));
+        }
+
+        private readonly TextEditorWrapper _editor;
+
+        public SelectableTextBlock()
+        {
+            _editor = TextEditorWrapper.CreateFor(this);
+        }
+    }
+}
---
+++ LogMergeRx/MVVM/TextEditorWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LogMergeRx.MVVM
+{
+    /// <summary>
+    /// https://stackoverflow.com/a/45627524/7156760
+    /// </summary>
+    public class TextEditorWrapper
+    {
+        private static readonly Type _textEditorType = Type.GetType("System.Windows.Documents.TextEditor, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+        private static readonly PropertyInfo _isReadOnlyProp = _textEditorType.GetProperty("IsReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly PropertyInfo _textViewProp = _textEditorType.GetProperty("TextView", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly MethodInfo _registerMethod = _textEditorType.GetMethod("RegisterCommandHandlers", BindingFlags.Static | BindingFlags.NonPublic, null, new[] { typeof(Type), typeof(bool), typeof(bool), typeof(bool) }, null);
+        private static readonly Type _textContainerType = Type.GetType("System.Windows.Documents.ITextContainer, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+        private static readonly PropertyInfo _textContainerTextViewProp = _textContainerType.GetProperty("TextView");
+
+        private static readonly PropertyInfo _textContainerProp = typeof(TextBlock).GetProperty("TextContainer", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static void RegisterCommandHandlers(Type controlType, bool acceptsRichContent, bool readOnly, bool registerEventListeners)
+        {
+            _registerMethod.Invoke(null, new object[] { controlType, acceptsRichContent, readOnly, registerEventListeners });
+        }
+
+        public static TextEditorWrapper CreateFor(TextBlock tb)
+        {
+            var textContainer = _textContainerProp.GetValue(tb);
+
+            var editor = new TextEditorWrapper(textContainer, tb, false);
+            _isReadOnlyProp.SetValue(editor._editor, true);
+            _textViewProp.SetValue(editor._editor, _textContainerTextViewProp.GetValue(textContainer));
+
+            return editor;
+        }
+
+        private readonly object _editor;
+
+        public TextEditorWrapper(object textContainer, FrameworkElement uiScope, bool isUndoEnabled)
+        {
+            _editor = Activator.CreateInstance(_textEditorType, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.CreateInstance,
+                null, new[] { textContainer, uiScope, isUndoEnabled }, null);
+        }
+    }
+}
