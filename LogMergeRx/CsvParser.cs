83a3f1a5c537ebfcde93061e3d23748a1d0a8a35
Valeri Hristov <valeri.hristov@inexto.com>
2023-01-18 09:31:16 +0100

    Cache threadIndex (perf optimization)

--- LogMergeRx/CsvParser.cs
+++ LogMergeRx/CsvParser.cs
@@ -13,7 +13,6 @@ namespace LogMergeRx
             new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 Delimiter = ";",
-                LeaveOpen = true,
                 BadDataFound = null,
                 //    MissingFieldFound = null,
             };
@@ -25,30 +24,34 @@ namespace LogMergeRx
         private static IEnumerable<LogEntry> ReadToEnd(Stream stream, FileId fileId)
         {
             using var textReader = new StreamReader(stream, leaveOpen: true);
-            using var csv = new CsvHelper.CsvReader(textReader, _configuration);
+            using var csv = new CsvHelper.CsvReader(textReader, _configuration, leaveOpen: true);
 
             if (stream.Position == 0 && csv.Read())
             {
                 csv.ReadHeader();
             }
 
+            int? threadOffset = null;
             while (csv.Read())
             {
                 var originalOffset = stream.Position;
                 LogEntry entry = null;
                 try
                 {
-                    var level = ParseLevel(csv.GetField<string>(2)?.Trim());
-                    // a new column ThreadId was added on index 2 at some point. We use this hacky way
-                    // to detect if it is present and offset the reset of the columns.
-                    var threadOffset = level == LogLevel.UNKNOWN ? 1 : 0;
+                    if (threadOffset == null)
+                    {
+                        var level = ParseLevel(csv.GetField<string>(2)?.Trim());
+                        // a new column ThreadId was added on index 2 at some point. We use this hacky way
+                        // to detect if it is present and offset the reset of the columns.
+                        threadOffset = level == LogLevel.UNKNOWN ? 1 : 0;
+                    }
 
                     entry = LogEntry.Create(
                         fileId: fileId,
                         date: csv.GetField<string>(0),
-                        level: ParseLevel(csv.GetField<string>(2 + threadOffset)?.Trim()),
-                        source: csv.GetField<string>(3 + threadOffset)?.Trim(),
-                        message: csv.GetField<string>(4 + threadOffset) + (csv.TryGetField<string>(5 + threadOffset, out var exceptionMessage) && exceptionMessage.Length > 0 ? $"\r\n{exceptionMessage}" : string.Empty));
+                        level: ParseLevel(csv.GetField<string>(2 + threadOffset.Value)?.Trim()),
+                        source: csv.GetField<string>(3 + threadOffset.Value)?.Trim(),
+                        message: csv.GetField<string>(4 + threadOffset.Value) + (csv.TryGetField<string>(5 + threadOffset.Value, out var exceptionMessage) && exceptionMessage.Length > 0 ? $"\r\n{exceptionMessage}" : string.Empty));
                 }
                 catch
                 {
